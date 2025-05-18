3f0ee3fc200d7d2a62d8343894540688c9d34489
amatejkova <annmatejkova@email.cz>
2025-05-18 20:50:45 +0200

    doplnění nahraní dat a balíčků

--- syntax_R/Nepiti_analyza.R
+++ syntax_R/Nepiti_analyza.R
@@ -1,4 +1,15 @@
 
+packages <- c("tidyverse", "haven", "DescTools", "GGally", "skimr", "dplyr",
+              "psych", "car", "FSA", "psych", "labelled")
+purrr::walk(packages, library, character.only = TRUE)
+
+
+data <- read_sav(file = "Data/Alkohol 2025_v01.sav") %>%    
+  mutate(across(where(is.labelled), as_factor))
+data_labelled <- generate_dictionary(data)
+
+
+
 # Jak rozšířená je krátkodobá  abstinence v české společnosti? ------------
 
 
