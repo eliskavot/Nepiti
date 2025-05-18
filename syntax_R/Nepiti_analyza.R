47bd5e4f30adebcf86fb68b2e17257062add16ab
amatejkova <annmatejkova@email.cz>
2025-05-18 20:49:03 +0200

    vytvoření společného syntaxu na analýzu

---
+++ syntax_R/Nepiti_analyza.R
@@ -0,0 +1,42 @@
+
+# Jak rozšířená je krátkodobá  abstinence v české společnosti? ------------
+
+
+
+
+# V jakých částech společnosti je krátkodobá/dlouhodobá abstinence --------
+
+
+
+
+# Má krátkodobá abstinence efekt na snižování míry konzumace alkoh --------
+
+
+
+
+# Jaké jsou osobní důvody (motivace) občanů ČR ke krátkodobé absti --------
+
+
+
+
+# Co představuje překážky během krátkodobé abstinence? --------------------
+
+
+
+
+
+# Co představuje překážky vedoucí k předčasnému ukončení krátkodob --------
+
+
+
+
+
+# Jaká část obyvatel ČR se pokouší nějakým způsobem kontrolovaně o --------
+
+
+
+
+# Jaké strategie občané ČR využívají v oblasti kontrolovaného omez --------
+
+
+
