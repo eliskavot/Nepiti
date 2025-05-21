b1576750706a142d98c6ed99da926a48bbbf75ff
MartinSima7 <martinsima2001@seznam.cz>
2025-05-21 12:32:40 +0200

    nahravani noveho datasetu

--- syntax_R/Nepiti_analyza.R
+++ syntax_R/Nepiti_analyza.R
@@ -4,7 +4,7 @@ packages <- c("tidyverse", "haven", "DescTools", "GGally", "skimr", "dplyr",
 purrr::walk(packages, library, character.only = TRUE)
 
 
-data <- read_sav(file = "Data/Alkohol 2025_v01.sav") %>%    
+data <- read_sav(file = "Data/Alkohol 2025_v02.sav") %>%    
   mutate(across(where(is.labelled), as_factor))
 data_labelled <- generate_dictionary(data)
 
